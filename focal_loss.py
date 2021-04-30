4a72f9f4b6225bd5a4369587ac608a64b38f0408
Lishuangtao <lishuangtao@stu.xmu.edu.cn>
2021-04-30 13:59:35 +0800

    focal loss

--- focal_loss.py
+++ focal_loss.py
@@ -6,7 +6,7 @@ def focal_loss(outputs, targets, alpha_t=None, gamma=0.0):
 
     :param outputs:
     :param targets:
-    :param alpha_t: A list of weight for each class
+    :param alpha_t: A list of weights for each class
     :param gamma:
     :return:
     """
@@ -20,7 +20,7 @@ def focal_loss(outputs, targets, alpha_t=None, gamma=0.0):
     elif alpha_t is None and gamma != 0:
         ce_loss = torch.nn.functional.cross_entropy(outputs, targets, reduction='none')
         p_t = torch.exp(-ce_loss)
-        focal_loss = ((1 - p_t) ** gamma * ce_loss).mean()
+        focal_loss = ((1 - p_t) ** gamma * ce_loss).mean()  # mean over the batch
 
     elif alpha_t is not None and gamma != 0:
         ce_loss = torch.nn.functional.cross_entropy(outputs, targets, reduction='none')
@@ -28,6 +28,7 @@ def focal_loss(outputs, targets, alpha_t=None, gamma=0.0):
         ce_loss = torch.nn.functional.cross_entropy(outputs, targets,
                                                     weight=torch.tensor(alpha_t), reduction='none')
         focal_loss = ((1 - p_t) ** gamma * ce_loss).mean()  # mean over the batch
+
     return focal_loss
 
 
